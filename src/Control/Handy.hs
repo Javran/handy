d897488d55bbd7ee7868ecc4b8f3c1f9b8c4f251
Javran Cheng <Javran.c@gmail.com>
2014-04-13 12:45:03 -0400

    first commit

---
+++ src/Control/Handy.hs
@@ -0,0 +1,55 @@
+module Control.Handy
+    ( -- re-exported definitions
+      (>>>), (<<<)
+    , first, second
+    , guard
+    , on
+    , (<$>), (<*>)
+    , (<>)
+    , mconcat
+    , Endo(..)
+    , isJust, isNothing
+    , fromMaybe
+      -- new definitions
+    , count
+    , countNot
+    , modArg
+    , modRes
+    , (.:)
+    , concatMapM
+    )
+where
+
+import qualified Data.Traversable as T
+
+import Control.Applicative
+import Control.Arrow
+import Control.Monad
+import Data.Function
+import Data.Maybe
+import Data.Monoid
+
+-- | count how many elements in a list meets the condition
+count :: (a -> Bool) -> [a] -> Int
+count p = length . filter p
+
+-- | count how many elements in a list does not meet the condition
+countNot :: (a -> Bool) -> [a] -> Int
+countNot = (modArg . modRes) not count
+
+-- | semantic editor combinators: argument modifier
+modArg :: (a -> b) -> (b -> c) -> a -> c
+modArg = flip (.)
+
+-- | semantic editor combinators: result modifier
+modRes :: (a -> b) -> (c -> a) -> c -> b
+modRes = (.)
+
+-- | semantic editor combinators: binary function result modifier
+(.:) :: (a -> b) -> (c -> d -> a) -> c -> d -> b
+(.:) = modRes . modRes
+
+-- | monadic "concatMap"
+concatMapM :: (Monad m,T.Traversable t,Monad t) =>
+              (a -> m (t b)) -> (t a -> m (t b))
+concatMapM = liftM join .: T.mapM
